e481d3dd295c99d411fa2774dfbce6fd9e27b948
bit271 <bohdan.pakhar.work@gmail.com>
2025-01-21 12:02:57 +0200

    .pom add

---
+++ src/main/java/com/bit/sparestore/SparestoreApplication.java
@@ -0,0 +1,13 @@
+package com.bit.sparestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparestoreApplication.class, args);
+	}
+
+}
